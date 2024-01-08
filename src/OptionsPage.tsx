6ebd25d111df4a0caf13eadbbc671464e87d5070
zhihui1532450 <zhihui153245@outlook.com>
2024-01-08 22:46:49 +0800

    add option

---
+++ src/OptionsPage.tsx
@@ -0,0 +1,13 @@
+// OptionsPage.tsx
+import React from 'react';
+
+const OptionsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Options of SavePage</h1>
+      {/* Add your options controls here */}
+    </div>
+  );
+}
+
+export default OptionsPage;
---
+++ src/options.tsx
@@ -0,0 +1,11 @@
+// options.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OptionsPage from './OptionsPage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <OptionsPage /> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
